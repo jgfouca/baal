939ba45115d8d9bbeedff6376155caa59007d258
James Foucar <jfoucar2000@yahoo.com>
2011-03-09 22:27:34 -0700

    Forgot to remove

--- code/game/InterfaceConstants.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef InterfaceConstants_hpp
-#define InterfaceConstants_hpp
-
-#define PLAYER_INTERFACE_X 10
-#define PLAYER_INTERFACE_Y 10
-#define PLAYER_PORTRAIT_WIDTH 150
-#define PLAYER_PORTRAIT_HEIGHT 150
-
-#endif
